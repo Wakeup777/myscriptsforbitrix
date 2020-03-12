131816853f3a2d34fdfb9a586c3b4a66ea2fcd45
alex <wakeup777@yandex.ru>
2020-03-12 12:15:34 +0300

    add dbscript.sh

---
+++ dbscript.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cat dbconn.php   | awk '{if ($1=="$DBLogin") DBLogin=$3;
+else if ($1=="$DBPassword") DBPassword =$3;
+else if ($1=="$DBName") DBName =$3;
+else if ($1=="$DBHost") DBHost =$3;} END  {print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql" }' | sed 's/\"//g' | sed 's/\;//g'
