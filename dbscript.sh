2c2de1b377fa0bafd17155236b71685d8fa35d12
Fonov Alex <wakeup777@yandex.ru>
2020-03-12 12:37:57 +0300

    some refactoring code

--- dbscript.sh
+++ dbscript.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
-cat dbconn.php   | awk '{if ($1=="$DBLogin") DBLogin=$3;
-else if ($1=="$DBPassword") DBPassword =$3;
-else if ($1=="$DBName") DBName =$3;
-else if ($1=="$DBHost") DBHost =$3;} END  {print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql" }' | sed 's/\"//g' | sed 's/\;//g'
+cat dbconn.php  |
+     awk '{
+             if ($1=="$DBLogin") DBLogin=$3;
+             else if ($1=="$DBPassword") DBPassword =$3;
+             else if ($1=="$DBName") DBName =$3;
+             else if ($1=="$DBHost") DBHost =$3;
+          } END {
+             print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql"
+          }'      | 
+    sed 's/\"//g' |
+    sed 's/\;//g'
