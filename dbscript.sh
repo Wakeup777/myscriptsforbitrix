e78854bb6674905aaf4875a6e3439d77d5f6b353
Fonov Alex <wakeup777@yandex.ru>
2020-03-25 12:52:27 +0300

    fix

--- dbscript.sh
+++ dbscript.sh
@@ -10,3 +10,56 @@ cat bitrix/php_interface/dbconn.php  |
           }'      | 
     sed 's/\"//g' |
     sed 's/\;//g'
+
+
+cat bitrix/php_interface/dbconn.php  | awk '{ if ($1=="$DBLogin") DBLogin=$3; else if ($1=="$DBPassword") DBPassword =$3; else if ($1=="$DBName") DBName =$3;  else if ($1=="$DBHost") DBHost =$3;} END { print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql" }'| sed 's/\"//g' | sed 's/\;//g'
+
+
+
+function dbi       
+              cat bitrix/php_interface/dbconn.php  |
+     awk '{
+             if ($1=="$DBLogin") DBLogin=$3;
+             else if ($1=="$DBPassword") DBPassword =$3;
+             else if ($1=="$DBName") DBName =$3;
+             else if ($1=="$DBHost") DBHost =$3;
+          } END {
+             print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql"
+          }'      | 
+    sed 's/\"//g' |
+    sed 's/\;//g'
+                       end
+
+function dbi 
+cat bitrix/php_interface/dbconn.php  | awk '{ if ($1=="$DBLogin") DBLogin=$3; else if ($1=="$DBPassword") DBPassword =$3; else if ($1=="$DBName") DBName =$3;  else if ($1=="$DBHost") DBHost =$3;} END { print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql" }'| sed 's/\"//g' | sed 's/\;//g'
+end
+
+
+#! /bin/bash
+dat=$(date +%Y-%m-%d_%H)
+
+cat bitrix/php_interface/dbconn.php  |
+     awk '{
+             if ($1=="$DBLogin") DBLogin=$3;
+             else if ($1=="$DBPassword") DBPassword =$3;
+             else if ($1=="$DBName") DBName =$3;
+             else if ($1=="$DBHost") DBHost =$3;
+          } END {
+              print "mysqldump -u"DBLogin" -p**** "DBName" -h"DBHost " > " DBName".sql"
+         }'      |
+    sed 's/\"//g' |
+    sed 's/\;//g'
+
+cat bitrix/php_interface/dbconn.php  |
+     awk '{
+             if ($1=="$DBLogin") DBLogin=$3;
+             else if ($1=="$DBPassword") DBPassword =$3;
+             else if ($1=="$DBName") DBName =$3;
+             else if ($1=="$DBHost") DBHost =$3;
+          } END {
+             print "mysqldump -u"DBLogin" -p"DBPassword" "DBName" -h"DBHost " > " DBName".sql"
+          }'      |
+    sed 's/\"//g' |
+    sed 's/\;//g' | sh
+
+tail -n1 *.sql
